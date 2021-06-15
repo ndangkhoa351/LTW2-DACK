baffa3b16ecd03f887b3241550af53537d9342a3
Nguyen Dang Khoa <ndangkhoa351@gmail.com>
2021-06-15 22:23:00 +0700

    Delete v2/WebPrograming2_Final-Project directory

--- v2/WebPrograming2_Final-Project/src/routers/User/auth/logout.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-router.get('/', (req, res) => {
-    delete req.session.userID;          //delete current user session.
-    res.render('User/auth/logout');
-});
-
-module.exports = router;
