0f4109d7eeaee79fa61ad62b6504a560c444bc13
huutinhluu <huutinhluu@gmail.com>
2021-07-06 22:51:22 +0700

    edit gui

--- v3/WebPrograming2_Final-Project/src/config/database.js
+++ v3/WebPrograming2_Final-Project/src/config/database.js
@@ -2,7 +2,7 @@ const { Sequelize } = require('sequelize');
 
 const sequelize = new Sequelize(
     process.env.DATABASE_URL ||
-    "postgres://postgres:1234@localhost:5432/final_project",
+    "postgres://postgres:postgres@localhost:5432/final_project",
     {
         dialect: 'postgres',
         dialectOptions: {},
